f4baf22e9d3dd725fb0ba7f8abf416572481856d
banushasarma <sharmabanu03@gmail.com>
2024-06-08 18:59:35 +0530

    Add project files.

---
+++ BlazorCrud.Infrastructure/Context/BlazorCrudDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BlazorCrud.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCrud.Infrastructure.Context
+{
+    public class BlazorCrudDbContext : DbContext
+    {
+        public BlazorCrudDbContext(DbContextOptions<BlazorCrudDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BlazorCrud.Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using BlazorCrud.Application.Interfaces;
+using BlazorCrud.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCrud.Infrastructure.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BlazorCrudDbContext context;
+        public BookRepository(IDbContextFactory<BlazorCrudDbContext> factory)
+        {
+            context = factory.CreateDbContext();
+        }
+    }
+}
